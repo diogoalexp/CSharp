3c5191cce15cbeaba02be15e63ec6821c647d86c
Diogo <diogoalexp@outlook.com>
2024-08-31 11:01:36 -0400

    Member Class definition

---
+++ Basics/Concepts/3.Members.cs
@@ -0,0 +1,33 @@
+﻿namespace Basics.Concepts
+{
+    public class MembersClass
+    {
+        sbyte System_SByte; //The sbyte type represents signed 8-bit integers with values from -128 to 127, inclusive.
+        
+        byte System_Byte; //The byte type represents unsigned 8-bit integers with values from 0 to 255, inclusive
+
+        short System_Int16;//The short type represents signed 16-bit integers with values from -32768 to 32767, inclusive.
+
+        ushort System_UInt16;//The ushort type represents unsigned 16-bit integers with values from 0 to 65535, inclusive.
+
+        int System_Int32; //The int type represents signed 32-bit integers with values from -2147483648 to 2147483647, inclusive.
+
+        uint System_UInt32; //The uint type represents unsigned 32-bit integers with values from 0 to 4294967295, inclusive.
+
+        long System_Int64; //The long type represents signed 64-bit integers with values from -9223372036854775808 to 9223372036854775807, inclusive.
+
+        ulong System_UInt64; //The ulong type represents unsigned 64-bit integers with values from 0 to 18446744073709551615, inclusive.
+
+        float System_Single; //The float type can represent values ranging from approximately 1.5 × 10⁻⁴⁵ to 3.4 × 10³⁸ with a precision of 7 digits.
+
+        double System_Double; //The double type can represent values ranging from approximately 5.0 × 10⁻³²⁴ to 1.7 × 10³⁰⁸ with a precision of 15-16 digits.        
+
+        decimal System_Decimal; //The decimal type is a 128-bit data type suitable for financial and monetary calculations. The decimal type can represent values including those in the range at least -7.9 × 10⁻²⁸ to 7.9 × 10²⁸, with at least 28-digit precision.
+
+        bool System_Boolean; //The bool type represents Boolean logical quantities. The possible values of type bool are true and false. 
+
+        char System_Char; //Stores a single character/letter, surrounded by single quotes
+
+        string System_String;//Stores a sequence of characters, surrounded by double quotes
+    }
+}
