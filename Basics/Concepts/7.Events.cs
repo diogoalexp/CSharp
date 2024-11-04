120fc638938e92cb2319c6b0ccb1fa9560bb9771
Diogo Pereira <diogo.pereira@ezoqc.com>
2024-11-04 08:19:12 -0500

    Events

---
+++ Basics/Concepts/7.Events.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+
+namespace Basics.Concepts
+{
+
+    
+    public class Events
+    {
+        public string StartEvent()
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.steps = new StringBuilder();
+
+            bl.steps.AppendLine("Create Event");
+
+            bl.StartProcess();
+
+            bl.steps.Append("End Event");
+
+            return bl.steps.ToString();
+        }
+        public static void bl_ProcessCompleted(object sender, SampleEventArgs e)
+        {
+            e.internalSteps.AppendLine("Step x: process bl_ProcessCompleted");
+
+            // some code here..
+        }
+    }
+
+    // event handler
+    public class ProcessBusinessLogic
+    {
+        // declaring an event using built-in EventHandler
+        public event EventHandler<SampleEventArgs> ProcessCompleted;
+        public StringBuilder steps;
+
+        public void StartProcess()
+        {
+            SampleEventArgs args = new SampleEventArgs() { internalSteps = steps};
+            args.internalSteps.AppendLine("Step 1: StartProcess");
+
+            // some code here..
+
+            OnProcessCompleted(args);
+        }
+
+        protected virtual void OnProcessCompleted(SampleEventArgs e)
+        {
+            EventHandler<SampleEventArgs> handler = ProcessCompleted;
+
+            if (handler != null)
+            {
+                e.internalSteps.AppendLine("Step 2: invoke bl_ProcessCompleted");
+                handler(this, e); //invoke bl_ProcessCompleted
+               
+            }
+
+            e.internalSteps.AppendLine("Step 3: invoke bl_ProcessCompleted");
+            ProcessCompleted?.Invoke(this, e); //invoke bl_ProcessCompleted
+
+            e.internalSteps.AppendLine("Step 4: End Process");
+        }
+    }
+
+    public class SampleEventArgs : EventArgs
+    {
+        public StringBuilder internalSteps { get; set; }
+    }
+}
---
+++ Test/Concepts/7.EventsTest.cs
@@ -0,0 +1,27 @@
+using Basics.Concepts;
+using System.Security.AccessControl;
+
+namespace Test.Concepts
+{
+    public class EventsTest
+    {
+        [Fact]
+        public void Events_EventHandler()
+        {
+            var expected_result = @"Create Event
+                Step 1: StartProcess
+                Step 2: invoke bl_ProcessCompleted
+                Step x: process bl_ProcessCompleted
+                Step 3: invoke bl_ProcessCompleted
+                Step x: process bl_ProcessCompleted
+                Step 4: End Process
+                End Event";
+
+            Events events = new Events();
+
+            var results = events.StartEvent();
+
+            Assert.Equal(expected_result.Replace(" ", ""), results.Replace(" ", ""));
+        }
+   }
+}
